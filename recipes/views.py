9e0e6d3544a3e844cafeb50d0137e227b65b8c36
booring37 <luvamaandongol@gmail.com>
2026-03-08 16:30:52 +0545

    Add CRUD views for Recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Recipe
+from .forms import RecipeForm
 
-# Create your views here.
+
+# LIST - Display all recipes
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+
+
+# READ - Display single recipe detail
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
+
+
+# CREATE - Add a new recipe
+def recipe_create(request):
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('recipe_list')
+    else:
+        form = RecipeForm()
+    return render(request, 'recipes/recipe_form.html', {'form': form, 'action': 'Add'})
+
+
+# UPDATE - Edit an existing recipe
+def recipe_update(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('recipe_detail', pk=recipe.pk)
+    else:
+        form = RecipeForm(instance=recipe)
+    return render(request, 'recipes/recipe_form.html', {'form': form, 'action': 'Edit'})
+
+
+# DELETE - Remove a recipe
+def recipe_delete(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    if request.method == 'POST':
+        recipe.delete()
+        return redirect('recipe_list')
+    return render(request, 'recipes/recipe_confirm_delete.html', {'recipe': recipe})
